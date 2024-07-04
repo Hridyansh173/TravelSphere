71166457f92a89895a6fa8c7564c0d5752d4f07f
Hridyansh173 <sridh429@gmail.com>
2024-07-04 10:19:50 +0530

    "created middleware"

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -1,17 +1,19 @@
 const jwt = require('jsonwebtoken');
+const config = require('config');
 
-module.exports = function (req, res, next) {
+module.exports = function(req, res, next) {
   // Get token from header
   const token = req.header('x-auth-token');
 
   // Check if not token
   if (!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied' });
+    return res.status(401).json({ msg: 'Authorization denied' });
   }
 
   // Verify token
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
     req.user = decoded.user;
     next();
   } catch (err) {
