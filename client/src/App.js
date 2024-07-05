791d04a966e791138e619ebdecc4aa30003b716d
Hridyansh173 <sridh429@gmail.com>
2024-07-05 23:41:08 +0530

    "minor fixes"

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,55 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import GuestAccessPage from './pages/GuestAccessPage';
 import HomePage from './pages/HomePage';
-import TripDetailsPage from './pages/TripDetailsPage';
-import BookingPage from './pages/BookingPage';
 import AboutPage from './pages/AboutPage';
+import BookingPage from './pages/BookingPage';
 import ContactUsPage from './pages/ContactUsPage';
+import ErrorPage from './components/ErrorPage';
+import TripDetailsPage from './pages/TripDetailsPage';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <div className="App">
-        <Navbar />
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/trips" element={<TripDetailsPage />} />
-          <Route path="/booking" element={<BookingPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactUsPage />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/guest-access" element={<GuestAccessPage />} />
+          <Route path="/home" element={<>
+            <Navbar />
+            <HomePage />
+            <Footer />
+          </>} />
+          <Route path="/about" element={<>
+            <Navbar />
+            <AboutPage />
+            <Footer />
+          </>} />
+          <Route path="/booking" element={<>
+            <Navbar />
+            <BookingPage />
+            <Footer />
+          </>} />
+          <Route path="/contact" element={<>
+            <Navbar />
+            <ContactUsPage />
+            <Footer />
+          </>} />
+          <Route path="/trips/:id" element={<>
+            <Navbar />
+            <TripDetailsPage />
+            <Footer />
+          </>} />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
